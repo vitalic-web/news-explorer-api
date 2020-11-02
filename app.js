35c627a5783f37be5f2667e540e4d74c25840bff
Vitalic-web <vitalic.web@gmail.com>
2020-11-02 16:00:19 +0200

    init package.json

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+const { PORT = 3000 } = process.env;
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
+});
